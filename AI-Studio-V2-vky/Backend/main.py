ae09412453058032b228c7c331b4742a75516278
vikrant200690 <vikrant200690@gmail.com>
2026-01-24 13:20:16 +0530

    Docker

--- AI-Studio-V2-vky/Backend/main.py
+++ AI-Studio-V2-vky/Backend/main.py
@@ -93,4 +93,8 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host=host, port=8077, reload=True) 
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
