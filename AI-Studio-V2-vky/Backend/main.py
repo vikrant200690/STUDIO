c897ec758013efd1bd60fab231631296706064ab
vikrant200690 <vikrant200690@gmail.com>
2026-01-27 12:35:07 +0530

    docker

--- AI-Studio-V2-vky/Backend/main.py
+++ AI-Studio-V2-vky/Backend/main.py
@@ -72,6 +72,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5177",
         "http://127.0.0.1:5177",
+        "https://your-vercel-app.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
